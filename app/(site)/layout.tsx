fa64caf9f863b306eed2cb8e0233a27c5cf7187f
rkd128 <165877735+rkd128@users.noreply.github.com>
2024-04-19 12:59:49 +0100

    Added logo

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -18,7 +18,9 @@ export default async function RootLayout({
     <html lang="en">
       <body className="mx-auto py-10">
         <header className="flex items-center justify-between px-20">
-          <Link href="/" className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-lg font-bold">Underexposed</Link>
+          <Link href="/" className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-lg font-bold">
+          <img src="/logo.svg" alt="Underexposed Logo" width="100" height="40" />
+          </Link>
           <div className="flex items-center gap-5 text-sm text-gray-600">
             {pages.map((page) => (
               <Link key={page._id} href={`/${page.slug}`} className="hover:underline">{page.title}</Link>
