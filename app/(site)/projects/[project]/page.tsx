64ae68ae4ac20013e0c3e3ce05cc0044d9d1350b
rkd128 <165877735+rkd128@users.noreply.github.com>
2024-04-22 11:17:41 -0500

    Image to img in project pages

--- app/(site)/projects/[project]/page.tsx
+++ app/(site)/projects/[project]/page.tsx
@@ -23,6 +23,6 @@ export default async function Project({ params }: Props) {
       <PortableText value={project.content} />
     </div>
 
-    <Image src={project.image} alt={project.name} width={1920} height={1080} className="mt-10 border-2 border-gray-700 object-cover rounded-xl" />
+    <img src={project.image} alt={project.name} width={1920} height={1080} className="mt-10 border-2 border-gray-700 object-cover rounded-xl" />
   </div>)
 } 
