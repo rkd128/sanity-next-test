7f3eeefb9f43edf1adcc720e967d4cd83dc11c35
rkd128 <165877735+rkd128@users.noreply.github.com>
2024-04-22 11:56:12 -0500

    feat: use single client instance

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -3,8 +3,10 @@ import { createClient, groq } from "next-sanity";
 import clientConfig from "./config/client-config";
 import { Page } from "@/types/Page";
 
+const client = createClient(clientConfig);
+
 export async function getProjects(): Promise<Project[]> {
-   return createClient(clientConfig).fetch(
+   return client.fetch(
         groq`*[_type == "project"]{
             _id,
             _createdAt,
@@ -18,7 +20,7 @@ export async function getProjects(): Promise<Project[]> {
 }
 
 export async function getProject(slug: string): Promise<Project> {
-    return createClient(clientConfig).fetch(
+    return client.fetch(
         groq`*[_type == "project" && slug.current == $slug][0]{
             _id,
             _createdAt,
@@ -33,18 +35,18 @@ export async function getProject(slug: string): Promise<Project> {
 }
 
 export async function getPages(): Promise<Page[]> {
-    return createClient(clientConfig).fetch(
+    return client.fetch(
         groq`*[_type == "page"]{
             _id,
             _createdAt,
             title,
             "slug": slug.current
-        }`,
-        {cache: 'no-store' }
+        }`
+        
     )
 }
 export async function getPage(slug: string): Promise<Page> {
-    return createClient(clientConfig).fetch(
+    return client.fetch(
         groq`*[_type == "page" && slug.current == $slug][0]{
             _id,
             _createdAt,
@@ -55,3 +57,4 @@ export async function getPage(slug: string): Promise<Page> {
         { slug }
     )
 }
+
